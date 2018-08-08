6eb779af9fb300929446f46c1afd3498d92675cf
orcrist57 <42181832+orcrist57@users.noreply.github.com>
2018-08-08 08:15:12 -0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-By setting the SCHEMA as FILM there's no need to refer to the SCHEME on each INSERT
+--By setting the SCHEMA as FILM there's no need to refer to the SCHEME on each INSERT
 SET SCHEMA = FILM;
 
 -- CLASIFICATION
